52103afc9cef99153b6971b6e707a7adbeebc300
KUNKUN2121 <42810210+KUNKUN2121@users.noreply.github.com>
2023-03-12 20:53:59 +0900

    範囲指定

--- main.py
+++ main.py
@@ -7,6 +7,38 @@ def onMouse(event, x, y, flags, params):
 
 
 img = cv2.imread('calender.jpg')
+
+height, width = img.shape[:2]
+print(height)  # 1241
+print(width)  # 1754
+
+half_height = int(height / 2)
+half_width = int(width / 2)
+
+
+
+cv2.line(img,
+    pt1=(half_width, 0),
+    pt2=(half_width, height),
+    color=(0, 255, 0),
+    thickness=3,
+    lineType=cv2.LINE_4,
+    shift=0)
+
+
+leftWidth = int(width / 16)
+rightWidth = int(width - leftWidth)
+upHeight = int(height / 5)
+downHeight = int(height - (height / 20))
+
+cv2.rectangle(img,
+              pt1=(leftWidth,upHeight),
+              pt2=(rightWidth,downHeight),
+              color=(0, 22, 20),
+              thickness=3,
+              lineType=cv2.LINE_4,
+              )
+
 cv2.imshow('Window', img)
 cv2.setMouseCallback('Window', onMouse)
 cv2.waitKey(0)
