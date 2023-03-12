1184503cb02713667dcff304230f1b56c7381da7
KUNKUN2121 <42810210+KUNKUN2121@users.noreply.github.com>
2023-03-12 20:54:08 +0900

    範囲指定

--- main.py
+++ main.py
@@ -12,20 +12,6 @@ height, width = img.shape[:2]
 print(height)  # 1241
 print(width)  # 1754
 
-half_height = int(height / 2)
-half_width = int(width / 2)
-
-
-
-cv2.line(img,
-    pt1=(half_width, 0),
-    pt2=(half_width, height),
-    color=(0, 255, 0),
-    thickness=3,
-    lineType=cv2.LINE_4,
-    shift=0)
-
-
 leftWidth = int(width / 16)
 rightWidth = int(width - leftWidth)
 upHeight = int(height / 5)
